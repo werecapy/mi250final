17f74f62e2bd4146cca6cfa9e7dc1fe19691eab2
Liz Koempel <koempele@msu.edu>
2025-10-31 00:39:22 -0400

    Added some bacheolors

---
+++ characters.py
@@ -0,0 +1,14 @@
+
+class Character:
+    def __init__(self, name,likes,location):
+        self.name = name
+        self.likes= likes
+        self.location = location
+
+sparty_likes= ["list","here"]
+sparty =Character("Sparty",sparty_likes,"East Lansing")
+
+terry_likes= ["list","here"]
+terry =Character("Terry",terry_likes,"Stanford")
+
+
