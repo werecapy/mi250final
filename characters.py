ee6a315e21b10dd0cc6faa1a735f6ad81bbff514
Liz Koempel <koempele@msu.edu>
2025-11-01 04:43:14 -0400

    Added some characters

--- characters.py
+++ characters.py
@@ -1,46 +1,104 @@
 
+#-----dict------
+PRONOUNS = {
+    "female": ["she", "her", "hers"],
+    "male": ["he", "him", "his"],
+    "nonbinary": ["they", "them", "theirs"]
+}
 
-pronouns_female = ["she", "her", "hers"]
-pronouns_male = ["he", "him", "his"]
-pronouns_nonbinary = ["they", "them", "theirs"]
 
+
+
+#-----classes
 class Character:
-    def __init__(self, name,likes,location,status):
+    def __init__(self, name,likes,location,status,job,exes):
         self.name = name
+        self.job = job
+        self.ex = exes
         self.likes= likes
         self.location = location
-        self.status = status
-        if status == 'bachelorette':
-            pronouns = pronouns_female
-        elif status == 'bachelor':
-            pronouns = pronouns_male
+        self.status = status.lower()
+        # Assign pronouns based on status
+        if self.status == "bachelorette":
+            self.pronouns = PRONOUNS["female"]
+        elif self.status == "bachelor":
+            self.pronouns = PRONOUNS["male"]
         else:
-            pronouns = pronouns_nonbinary
+            self.pronouns = PRONOUNS["nonbinary"]
 
-sparty_likes= ["list","here"]
+
+#------Characters--------
+sparty_exes =["list","here","lifting","creatine powder"]
+sparty_likes= ["list","here","lifting","creatine powder"]
 sparty =Character("Sparty",
                   sparty_likes,
                   "East Lansing",
-                  "bachelor")
-
+                  "bachelor",
+                  "mascot", sparty_exes)
+tereesa_exes = ["list","here"]
 tereesa_likes= ["list","here"]
 tereesa =Character("Tereesa",
                    tereesa_likes,
                    "Stanford",
-                   "bachelorette")
-
+                   "bachelorette"
+                   ,"mascot"
+                   ,tereesa_exes)
+donald_exes = ["list","here"]
 donald_likes= ["list","here"]
 donald = Character(
             "Donald",
             donald_likes,
             "Eugene",
-        "bachelor")
-
+        "bachelor",
+            "mascot",donald_exes)
+brutus_exes = ["list","here", "Animal the  muppet"]
 brutus_likes= ["list","here"]
-brutus = Character("Brutus", brutus_likes,"Columbus", "bachelor")
-
+brutus = Character("Brutus",
+                   brutus_likes,
+                   "Columbus",
+                   "bachelor",
+                  "mascot"
+                   ,brutus_exes)
+otto_exes = ["list","here", "Animal the  muppet"]
 otto_likes= ["list","here"]
 otto = Character("Otto",
                   otto_likes,
                  "Syracuse",
-                  "helor")
+                  "bachelorx",
+                  "mascot",
+                 otto_exes)
+keggy_exes = ["list","here", "Animal the  muppet"]
+keggy_likes= ["list","here"]
+keggy = Character("Keggy",
+                  keggy_likes,
+                  "Hanover",
+                  "bachelor",
+                  "mascot",
+                  keggy_exes)
+
+gritty_exes = ["list","here", "Animal the  muppet"]
+gritty_likes= ["list","here", ]
+gritty = Character("Gritty",
+                  gritty_likes,
+                  "Hanover",
+                  "bachelor",
+                  "mascot",
+                gritty_exes)
+
+blue_likes= ["list","here"]
+blue = Character("blue",
+                  blue_likes,
+                  "Hanover",
+                  "bachelor",
+                  "mascot",
+                 gritty_exes)
+
+
+willy_likes= ["list","here"]
+willy = Character("Willy the wave",
+                  willy_likes,
+                  "Hanover",
+                  "bachelor",
+                  "mascot",
+                 gritty_exes)
+#--------functions-----------
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 import os
+from characters import Character as ch
 
 # Lists of pronouns
 pronouns_female = ["she", "her", "hers"]
@@ -8,41 +9,63 @@ pronouns_nonbinary = ["they", "them", "theirs"]
 
 def greet():
     print("Welcome to Mascot Dating Simulator!")
-    name = input("What is your name? ")
-    print("Hello, " + name + "!")
-    gender = input("What is your gender? ").strip().lower()
+    user_name = input("What is your name? ")
+    print("Hello, " + user_name + "!")
+    user_input_gender = input("What is your gender? ").strip().lower()
 
-    if gender in ("female", "f"):
+    if user_input_gender in ("female", "f"):
+        user_input_gender_save = "female"
         user_gender = pronouns_female
-    elif gender in ("male", "m"):
+    elif user_input_gender in ("male", "m"):
+        user_input_gender_save = "male"
         user_gender = pronouns_male
-    elif gender in ("none", "nonbinary", "nb"):
+    elif user_input_gender in ("none", "nonbinary", "nb"):
+        user_input_gender_save = "nonbinary"
         user_gender = pronouns_nonbinary
     else:
         print("Gender not recognized, defaulting to neutral pronouns.")
+        user_input_gender_save = "nonbinary"
         user_gender = pronouns_nonbinary
 
     user_age = int(input("How old are you? "))
 
-    print(f"So your name is {name}, your gender is {user_gender}, and you are {user_age} years old.")
+    print(f"So your name is {user_name}, your gender is {user_input_gender_save}, and you are {user_age} years old.")
     input("Press enter to continue...")
 
-    return name, gender, user_gender, user_age
+    return user_name,user_input_gender, user_input_gender_save, user_gender, user_age
 
-def save(name, gender, user_gender, user_age):
+def save(user_name, user_input_gender,user_input_gender_save, user_gender, user_age):
     relative_directory = "Saves"
     os.makedirs(relative_directory, exist_ok=True)
-    filename = f"{name}.txt"
+    filename = f"{user_name}.txt"
     full_path = os.path.join(relative_directory, filename)
 
     with open(full_path, "a") as f:
-        f.write(f"This user's name is {name}.\n")
-        f.write(f"This user's gender is {gender}.\n")
+        f.write(f"This user's name is {user_name}.\n")
+        f.write(f"This user's gender is {user_input_gender_save}.\n")
         f.write(f"This user's pronouns are {user_gender}.\n")
         f.write(f"This user is {user_age} years old.\n")
 
-    print(f"User inputs saved to {full_path}")
+    print(f"User inputs saved to {full_path}!")
 
-# Run the game
-name, gender, user_gender, user_age = greet()
-save(name, gender, user_gender, user_age)
+def meet_the_bas(self):
+    #this should be the introduction function for whenever the
+    #user needs a refresh on who someone is.
+    pass
+
+
+
+
+#--------start the game
+
+
+user_name, user_input_gender, user_input_gender_save, user_gender, user_age = greet()
+save(user_name, user_input_gender, user_input_gender_save, user_gender, user_age)
+"""
+there's a mascot convention in EL and you just so happen to be
+a student at MSU. You need to run errands near breslin or whatever
+convention center it is at and you run into all of these mascots
+looking for love <3.
+"""
+
+
