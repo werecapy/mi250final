ab48fb6e08127739a031361f9b4e166339760ec7
Liz Koempel <koempele@msu.edu>
2025-12-11 02:01:35 -0500

    added more art

--- poster_screen.py
+++ poster_screen.py
@@ -1,9 +1,11 @@
 import turtle
 
 def poster_pic():
+    #shows poster in window
     poster_window= turtle.Screen()
     poster_window.register_shape('poster.gif')
     poster_window.title("Mascot Dating Simulator")
     poster_window.bgpic("poster.gif")
     turtle.exitonclick()
-turtle.mainloop()
+
+    poster_window.mainloop()
